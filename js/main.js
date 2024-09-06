2cf52217f999d237f6570a3117bc6388f2992cc6
bercun <bercun@gmail.com>
2024-09-06 11:04:32 -0300

    se reparo agregar al carrito pasando por fetch json, repara ocultar carrito vacio

--- js/main.js
+++ js/main.js
@@ -16,62 +16,60 @@ let despedida = 'Hasta la próxima';
 
 //? array inicial del carrito
 let carrito = JSON.parse(localStorage.getItem('local-carrito')) || []; 
+let productosEntrantes = [];
+let productosPrincipales = [];
+let carta =[];
 
-//? array de productos
-const productosEntrantes = [
-    {id: 1, 
-    nombre: 'Carpacio de gambas',
-    descripcion: 'Gambas de Denia con vinagreta de lima,rúcula y tomate cherry',
-    precio: 560,
-    },
-    {id: 2, 
-    nombre: 'Ensalada caprese',
-    descripcion: 'Tomate ecológico, muzarella de bufala,rúcula y aceite de oliva',
-    precio: 250,
-        },
-    {id: 3, 
-    nombre: 'Pan de queso',
-    descripcion: 'Pancitos recién horneados de queso',
-    precio: 550,
-    },
-    {id: 4,
-    nombre: 'Pan de molde con semillas',
-    descripcion: 'Pan de molde, de 600gr , con sésamo, lino, chía y avena',
-    precio: 550,
-    },
-    
-    
-];
-
-const productosPrincipales = [
-    {id:11, 
-    nombre: 'Raviolones de espinaca', 
-    descripcion: 'Raviolones rellenos de espinaca, ricotta y parmesano con masa de tomates secos', 
-    precio: 560},
-    {id:12,  
-    nombre: 'Lasaña de pollo y verduras',
-    descripcion: 'Lasaña artesanal, de supremas de pollos cocidas al tomillo y verduras de temporada',
-    precio: 250},
-    {id:13,
-    nombre: 'Milanesas de carne',
-    descripcion: 'Milanesas de novillo, empanadas con nuestra mezcla especial de pan rallado sin gluten',
-    precio: 550},
-    {id:14,
-    nombre: 'Strudel de verduras',
-    descripcion: 'Strudel, con masa de hojaldre artesanal y relleno de verduras de temporada',
-    precio: 550},
-];
-
-const carta = [...productosEntrantes, ...productosPrincipales];
 
 
 //! definimos variables de los elementos del DOM
-const tablaEntrantes = document.querySelector('#tablaEntrantes');
-const tablaPrincipales = document.querySelector('#tablaPrincipales');
+let tablaEntrantes = document.querySelector('#tablaEntrantes');
+let tablaPrincipales = document.querySelector('#tablaPrincipales');
 let btnAgregar = document.querySelectorAll('.btnAgregar');
-const nCarrito = document.querySelector('#carrito-box');
+let nCarrito = document.querySelector('#carrito-box');
 
 
+// //? array de productos
+
+
+
+//? definimos fetch para traer productos de un archivo json
+
+
+
+fetch ("../database/productosEn.json")
+    .then((response) =>  response.json())
+    .then((data) => {
+        productosEntrantes = data;
+        tablaEntrantes = document.querySelector('#tablaEntrantes');
+        // console.log(tablaEntrantes);
+        mostrarEntrantes();
+        carta = carta.concat(productosEntrantes);
+        btnAgregar = document.querySelectorAll('.btnAgregar');
+        btnAgregar.forEach((boton) => {
+            boton.addEventListener('click', agregarCarrito);
+            
+        });
+    
+    });
+
+    fetch ("../database/productosPr.json")
+    .then((response) =>  response.json())
+    .then((data) => {
+        productosPrincipales = data;
+        tablaPrincipales = document.querySelector('#tablaPrincipales');
+        // console.log(tablaEntrantes);
+        mostrarPrincipales();
+        carta = carta.concat(productosPrincipales);
+        btnAgregar = document.querySelectorAll('.btnAgregar');
+        btnAgregar.forEach((boton) => {
+            boton.addEventListener('click', agregarCarrito);
+            
+        });
+    
+    });    
+    
+;
 //! funciones para mostrar productos en la tabla
 function mostrarEntrantes() {
     if(tablaEntrantes) {
@@ -109,13 +107,14 @@ function mostrarPrincipales() {
         console.warn('no hay tabla de principales');
     }   
 }
-        
+
+
+
 
 //? llamado a las funciones
 mostrarEntrantes();
 mostrarPrincipales();
 updatebtnAgregar();
-// vaciarCarrito();
 syncnCarrito();
 localCarrito();
 
@@ -124,7 +123,6 @@ localCarrito();
 
 function updatebtnAgregar() {
     btnAgregar = document.querySelectorAll('.btnAgregar');
-    
     btnAgregar.forEach((boton) => {
     boton.addEventListener('click', agregarCarrito);
     });
@@ -133,7 +131,6 @@ function updatebtnAgregar() {
 function agregarCarrito() {
     let idProducto = this.id;
     // console.log( idProducto);
-    // console.log(this);
     eleccionDecompra = carta.find((producto) => producto.id == idProducto);
     if (carrito.some((producto) => producto.id == idProducto)) {
         eleccionDecompra.cantidad++;
